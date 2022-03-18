5048a204549adc50e57280dc72997bc32645c80a
Francisco Matelli <francisco@f5sites.com>
2022-03-18 19:44:20 -0300

    hook in content and better display

--- fm-giphy-search.php
+++ fm-giphy-search.php
@@ -11,43 +11,54 @@
  * Text Domain:       fm-giphy-search
  */
 
-add_action('wp_head', 'hook_plugin_in_head');
+add_action('the_content', 'hook_plugin_in_html');
 
-
-function hook_plugin_in_head() {
+function hook_plugin_in_html() {
 	$query_gif_input = $_POST["query_gif_input"];
+	#
+	echo '<h1>Gif Finder</h1>';
+	echo '<div class="container">';
+
+	#
 	output_search_input();
 	if(!isset($query_gif_input) || empty($query_gif_input) ) {
-		echo "Use the input to search for some wonderfull gifs!!!!";
+		echo "<p>Use the input to search for some wonderfull gifs!!!!</p>";
+		echo "<p>Checkout some trending gifts</p>";
+		fm_giphy_serch(NULL);
 	} else {
 		echo "<h2>Searching for $query_gif_input</h2>";
 		fm_giphy_serch($query_gif_input);
 	}
-	die;
+	echo '</div>';
+	#die;
 }
 
 function output_search_input() {
+	echo '<div class="row">';
 	echo '<form method="POST">';
 	echo '<input type="text" placeholder="enter gifs you want to see" name="query_gif_input">';
 	echo '<input type="submit" value="Search">';
 	echo '</form>';
+	echo '</div>';
 }
 
 function fm_giphy_serch($query_gif_input_received) {
-	$query_gif_input_received = urlencode( $query_gif_input_received );
-	$jsonurl = "https://api.giphy.com/v1/gifs/search?api_key=pLURtkhVrUXr3KG25Gy5IvzziV5OrZGa&q=$query_gif_input_received&limit=5";
+	if(!isset($query_gif_input_received) || empty($query_gif_input_received)) {
+		$jsonurl = "https://api.giphy.com/v1/gifs/trending?api_key=pLURtkhVrUXr3KG25Gy5IvzziV5OrZGa&limit=12";
+	} else {
+		$query_gif_input_received = urlencode( $query_gif_input_received );
+		$jsonurl = "https://api.giphy.com/v1/gifs/search?api_key=pLURtkhVrUXr3KG25Gy5IvzziV5OrZGa&q=$query_gif_input_received&limit=12";
+	}
 	$json = file_get_contents($jsonurl);
 	$json_d = json_decode($json, true);	
-	echo '<div class="container">';
+	
 	echo '<div class="row">';
-
 	foreach ($json_d['data'] as $k => $v) {
 		$img_src = $v["images"]["downsized"]["url"];
-		echo '<div class="col-sm">';
+		echo '<div class="col-lg-3 col-md-4 col-sm-6">';
 		echo "<img src=$img_src>";
 		echo '</div>';
 	}
-	echo '</div>';
-	echo '</div>';
+	echo '</div>';	
 }
 ?>
